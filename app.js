7d83b7aa494da72b492d056d1cfdaf12d496d66e
Selvan <69357513+Selvan-DEV@users.noreply.github.com>
2025-03-31 14:59:51 +0530

    New connection code added

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 // Enable CORS for all origins (be cautious with this in production)
 app.use(cors());
