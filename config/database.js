0a5582bd15a22bebedb433023cfed80c3e66c7ab
Selvan <69357513+Selvan-DEV@users.noreply.github.com>
2025-05-24 21:37:47 +0530

    DB connect changed

--- config/database.js
+++ config/database.js
@@ -7,12 +7,20 @@ const mysql = require('mysql2');
 //   database: 'tutorial-app-db'
 // });
 
+// const pool = mysql.createPool({
+//   host: 'turntable.proxy.rlwy.net',
+//   user: 'root',
+//   password: 'fPUkYJoRrenwmSlNRxCztTxVJOeSUTVC',
+//   database: 'railway',
+//   port: 10244
+// });
+
 const pool = mysql.createPool({
-  host: 'turntable.proxy.rlwy.net',
+  host: '89.116.21.79',
   user: 'root',
-  password: 'fPUkYJoRrenwmSlNRxCztTxVJOeSUTVC',
-  database: 'railway',
-  port: 10244
+  password: 'Nilaash@2020',
+  database: 'asianpaints_db',
+  port: 3306
 });
 
 module.exports = pool.promise();
